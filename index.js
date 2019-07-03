3c19ebc606e0f73e93dde1d660910b220af2c609
Jordan Winslow <admin@jordanwinslow.me>
2019-07-03 02:32:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
-  // assign the contents of the existing object to a new object, then add the new key and value to it, preserving the old object.
+  
+  /* assign the contents of the existing object to a new object, then add the new key and value to it, preserving the old object.
+  
+  The first value of Object.assign() is the target object that gets modified, which is why we are sending it a blank object "{}" instead of our variable "object" */
 }
