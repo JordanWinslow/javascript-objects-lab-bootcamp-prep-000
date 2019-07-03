dd3d3738278531e5a352f248c42554ac94383016
Jordan Winslow <admin@jordanwinslow.me>
2019-07-03 02:10:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // define our recipes object to receive content
-var recipes = new Object()
+recipes = new Object()
 
 function updateObjectWithKeyAndValue(object, key, value){
   object[key] = value
