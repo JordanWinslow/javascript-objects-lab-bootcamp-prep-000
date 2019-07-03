5e4a3765f8a7402313900b96d4d045406e373e48
Jordan Winslow <admin@jordanwinslow.me>
2019-07-03 02:30:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, {[key]: value})
+  // assign the contents of the existing object to a new object, then add the new key and value to it, preserving the old object.
 }
