91007216c2f233e3871d4227eaf4f02f0fc4990e
Jordan Winslow <admin@jordanwinslow.me>
2019-07-03 02:17:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  let updatedRecipe = Object.assign({}, {[key]: [value]})
+  let updatedRecipe = Object.assign(object,)
   return updatedRecipe
 }
